80a3ca724ec99d093f5bc78538c87691d056afdb
edwinjsalas <edwinjsalas1@gmail.com>
2024-01-28 22:32:17 -0800

    added new code wars challenge

---
+++ 7kyu/Reverse-Words.js
@@ -0,0 +1,14 @@
+// DESCRIPTION:
+// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+// Examples
+// "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces"      ==> "elbuod  secaps"
+
+//MY SOLUTION: 
+function reverseWords(str) {
+   
+   let reverseWordArr = str.split(' ').map((word ) => word.split('').reverse().join(''))
+  return reverseWordArr.join(' ')
+  }
+  

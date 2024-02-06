0230b856917fd9c20e8db12a800515b682ab798d
edwinjsalas <edwinjsalas1@gmail.com>
2024-02-05 22:05:01 -0800

    added new code wars challenge

---
+++ 8kyu/Squaring-argument.js
@@ -0,0 +1,9 @@
+// DESCRIPTION:
+
+// Now you have to write a function that takes an argument and returns the square of it.
+
+// MY SOLUTION:
+
+function square (number){
+    return number ** 2
+  }
